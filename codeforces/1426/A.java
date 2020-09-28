923eff369f572203047657e0e09d9cf7a331cdd3
Aditya Aggarwal <adityaaggarwal7861@gmail.com>
2020-09-28 13:42:00 +0530

    Add solution for problem `A - Floor Number` Link: https://codeforces.com/contest/1426/problem/A Tags: implementation, math, *800 Ref: https://codeforces.com/contest/1426/submission/94066182

---
+++ codeforces/1426/A.java
@@ -0,0 +1,342 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static class FastReader
+    {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader()
+        {
+            br = new BufferedReader(new
+                    InputStreamReader(System.in));
+        }
+
+        String next()
+        {
+            while (st == null || !st.hasMoreElements())
+            {
+                try
+                {
+                    st = new StringTokenizer(br.readLine());
+                }
+                catch (IOException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt()
+        {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong()
+        {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble()
+        {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine()
+        {
+            String str = "";
+            try
+            {
+                str = br.readLine();
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+    static class Reader
+    {
+        final private int BUFFER_SIZE = 1 << 16;
+        private DataInputStream din;
+        private byte[] buffer;
+        private int bufferPointer, bytesRead;
+
+        public Reader()
+        {
+            din = new DataInputStream(System.in);
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+
+        public Reader(String file_name) throws IOException
+        {
+            din = new DataInputStream(new FileInputStream(file_name));
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+
+        public String readLine() throws IOException
+        {
+            byte[] buf = new byte[64]; // line length
+            int cnt = 0, c;
+            while ((c = read()) != -1)
+            {
+                if (c == '\n')
+                    break;
+                buf[cnt++] = (byte) c;
+            }
+            return new String(buf, 0, cnt);
+        }
+
+        public int nextInt() throws IOException
+        {
+            int ret = 0;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+            do
+            {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0' && c <= '9');
+
+            if (neg)
+                return -ret;
+            return ret;
+        }
+
+        public long nextLong() throws IOException
+        {
+            long ret = 0;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+            do {
+                ret = ret * 10 + c - '0';
+            }
+            while ((c = read()) >= '0' && c <= '9');
+            if (neg)
+                return -ret;
+            return ret;
+        }
+
+        public double nextDouble() throws IOException
+        {
+            double ret = 0, div = 1;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+
+            do {
+                ret = ret * 10 + c - '0';
+            }
+            while ((c = read()) >= '0' && c <= '9');
+
+            if (c == '.')
+            {
+                while ((c = read()) >= '0' && c <= '9')
+                {
+                    ret += (c - '0') / (div *= 10);
+                }
+            }
+
+            if (neg)
+                return -ret;
+            return ret;
+        }
+
+        private void fillBuffer() throws IOException
+        {
+            bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+            if (bytesRead == -1)
+                buffer[0] = -1;
+        }
+
+        private byte read() throws IOException
+        {
+            if (bufferPointer == bytesRead)
+                fillBuffer();
+            return buffer[bufferPointer++];
+        }
+
+        public void close() throws IOException
+        {
+            if (din == null)
+                return;
+            din.close();
+        }
+    }
+
+    static long MOD = (long) (1e9 + 7);
+
+    static long powerLL(long x, long n)
+    {
+        long result = 1;
+        while (n > 0)
+        {
+            if (n % 2 == 1)
+            {
+                result = result * x % MOD;
+            }
+            n = n / 2;
+            x = x * x % MOD;
+        }
+        return result;
+    }
+
+    static long powerStrings(String sa, String sb)
+    {
+        long a = 0, b = 0;
+
+        for (int i = 0; i < sa.length(); i++)
+        {
+            a = (a * 10 + (sa.charAt(i) - '0')) %
+                    MOD;
+        }
+
+        for (int i = 0; i < sb.length(); i++)
+        {
+            b = (b * 10 + (sb.charAt(i) - '0')) %
+                    (MOD - 1);
+        }
+
+        return powerLL(a, b);
+    }
+
+    static long gcd(long a, long b)
+    {
+        if (a==0) return b;
+        else return gcd(b%a,a);
+    }
+
+    static long lcm(long a, long b)
+    {
+        return (a*b)/gcd(a,b);
+    }
+
+    static int lower_bound(List<Integer> list, int k)
+    {
+        int s = 0;
+        int e = list.size();
+
+        while (s!=e)
+        {
+            int mid = (s+e)>>1;
+
+            if (list.get(mid)<k) s = mid+1;
+            else e = mid;
+        }
+
+        if (s == list.size()) return -1;
+        return s;
+    }
+
+    static int upper_bound(List<Integer> list, int k)
+    {
+        int s = 0;
+        int e = list.size();
+
+        while (s!=e)
+        {
+            int mid = (s+e)>>1;
+
+            if (list.get(mid)<=k) s = mid+1;
+            else e = mid;
+        }
+
+        if (s == list.size()) return -1;
+        return s;
+    }
+
+    static void addEdge(ArrayList<ArrayList<Integer>>graph, int edge1, int edge2)
+    {
+        graph.get(edge1).add(edge2);
+        graph.get(edge2).add(edge1);
+    }
+
+    public static HashMap<Long, Long> sortByValue(HashMap<Long, Long> hm)
+    {
+
+        List<Map.Entry<Long, Long> > list = new LinkedList<Map.Entry<Long, Long> >(hm.entrySet());
+
+        // Sort the list
+        Collections.sort(list, new Comparator<Map.Entry<Long, Long> >() {
+            public int compare(Map.Entry<Long, Long> o1, Map.Entry<Long, Long> o2)
+            {
+                return (o1.getValue()).compareTo(o2.getValue());
+            }
+        });
+
+        // put data from sorted list to hashmap
+        HashMap<Long, Long> temp = new LinkedHashMap<Long, Long>();
+        for (Map.Entry<Long, Long> aa : list) {
+            temp.put(aa.getKey(), aa.getValue());
+        }
+        return temp;
+    }
+
+    public static void main(String[] args) throws java.lang.Exception
+    {
+        try
+        {
+            //FastReader fr = new FastReader();
+            Reader fr = new Reader();
+
+            try(OutputStream out = new BufferedOutputStream(System.out))
+            {
+                int testcases = fr.nextInt();
+
+                while (testcases -- > 0)
+                {
+                    int n = fr.nextInt();
+                    int x = fr.nextInt();
+
+                    if (n == 1 || n == 2) out.write((1+"\n").getBytes());
+                    else
+                    {
+                        int count = 1;
+                        int ele = 2;
+
+                        while (ele<n)
+                        {
+                            ele+=x;
+                            count++;
+                        }
+                        /*
+                                1 2
+                                3 4 5 6 7
+                                8 9 10 11 12
+                                13 14 15 16 17
+                                18 19 20 21 22
+                         */
+
+                        out.write((count+"\n").getBytes());
+                    }
+                }
+
+                out.flush();
+            }
+
+        }
+
+        catch(Exception e){}
+        finally{}
+    }
+}
+
+
+//(()) () (()((()()()())))
